9d4290fadcb0f990817e4b57f39c51dab0d5d110
Raadiyah Arend <raadiyaharend6@gmail.com>
2023-05-25 15:24:01 +0200

    attempt 4

--- taxi-queue.dom.js
+++ taxi-queue.dom.js
@@ -1,11 +1,59 @@
-// write your DOM code here.
-
-
 // DOM element references
+const TaxiQueue = TaxiQueue();//factory function 
 
-// create Factory Function instance
+const passengerValueDisplay = document.querySelector(".counter");//Value display of the passneger amount counter
 
-const taxiQueue = TaxiQueue();
+const joinBtnPassengers = document.querySelector(".join_queue");//button to join the queue
 
-// DOM events
+const leaveBtn = document.querySelector(".leave_queue");//button to leave the PASSENGER queue
+
+const taxiQueValueDisplay = document.querySelector(".taxi_queue_count count");//Value display of the TAXI amount counter
+
+const joinQueBtnTaxi = document.querySelector("join_taxi_queue");//button for Taxi queue
+
+const departBtn = document.querySelector(".depart");//depart button
+
+const myDepartMessage = document.querySelector(".departMessage");//depart message alert
+
+let counterPassenger = 0;
+
+let counterTaxi = 0;
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+
+joinBtnPassengers.addEventListener("click", ()=>{
+    counterPassenger++;
+    passengerValueDisplay.innerHTML = counterPassenger;
+})
+
+leaveBtn.addEventListener("click", ()=>{
+    counterPassenger--;
+    counterPassenger >= 0;
+    passengerValueDisplay.innerHTML = counterPassenger;
+})
+
+// What im tryna say is that that the join button needs to continously add peeps. 
+//button needs to continously minus someone so they can join a taxi. 
+
+///////////////////////////////////////////////////////////////////////////////////////
+joinQueBtnTaxi.addEventListener("click", () => {
+    
+
+    
+    counterPassenger ++;
+    taxiQueValueDisplay >= 12;
+    taxiQueValueDisplay.innerHTML = counterPassenger;
+})
+
+/////////////////////////////////////////////////////////////////////////////////////////
+departBtn.addEventListener("click", () => {
+   
+})
+
+
+
+
+
+
 
--- taxi-queue.js
+++ taxi-queue.js
@@ -1,28 +1,43 @@
 function TaxiQueue() {
 
+	let passengerCounter = 0;
+
+	let taxiCounter = 0;
+
 
 	function joinQueue() {
+
+		passengerCounter++;
 
 	}
 
 	function leaveQueue() {
 
+		 passengerCounter--;
+
 	}
 
 	function joinTaxiQueue() {
+		taxiCounter++;
 
 	}
 
 	function queueLength() {
-
+		passengerCounter;
+	
 	}
 
 	function taxiQueueLength() {
-
+		taxiCounter;
 	}
 
-	function taxiDepart(){
+	function taxiDepart() {
 
+		if (taxiQueue > 0 && passengerQueue >= 12 ) {
+			taxiQueue--;
+		
+		  }
+		
 	}
 
 	return {
